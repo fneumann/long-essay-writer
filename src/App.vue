3eb5d448896a94a699382688747594fcca656a73
Fred Neumann <fred.neumann@fau.de>
2022-01-28 03:44:53 +0100

    added elements

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
 <template>
-  <div id="app">
-    <tiny-example></tiny-example>
-    <p>initialized: {{initialized}}</p>
-    <p>backendUrl: {{backendUrl}}</p>
-    <p>returnUrl: {{returnUrl}}</p>
-    <p>userKey: {{userKey}}</p>
-    <p>environmentKey: {{environmentKey}}</p>
-    <p>authToken: {{authToken}}</p>
-  </div>
+  <v-app>
+    <v-app>
+      <v-app-bar elevation="1" color="white" density="compact" >
+          <v-app-bar-title>Klausur Vertragsrecht</v-app-bar-title>
+          <v-spacer></v-spacer>
+
+
+        <v-btn icon density="compact">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-navigation-drawer elevation="2" color="grey-lighten-1" rail expand-on-hover>
+
+        <v-list>
+          <v-list-item prepend-icon="mdi-text-box-outline" title="Aufgabenstellung"></v-list-item>
+          <v-list-item prepend-icon="mdi-file-edit-outline" title="Essay schreiben"></v-list-item>
+          <v-list-item prepend-icon="mdi-clipboard-outline" title="Notizbrett 1"></v-list-item>
+          <v-list-item prepend-icon="mdi-clipboard-outline" title="Notizbrett 2"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container>
+          <v-row dense>
+            <v-col cols="6">
+              <v-card>
+                <v-list-item>initialized: {{initialized}}</v-list-item>
+                <v-list-item>backendUrl: {{backendUrl}}</v-list-item>
+                <v-list-item >returnUrl: {{returnUrl}}</v-list-item>
+                <v-list-item>userKey: {{userKey}}</v-list-item>
+                <v-list-item>environmentKey: {{environmentKey}}</v-list-item>
+                <v-list-item>authToken: {{authToken}}</v-list-item>
+              </v-card>
+            </v-col>
+            <v-col cols="6">
+              <tiny-example></tiny-example>
+            </v-col>
+          </v-row>
+        </v-container>
+
+
+      </v-main>
+    </v-app>
+  </v-app>
 </template>
 
 <script>
