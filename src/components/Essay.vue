21d832c9652f3200aad9e487c769c36429097259
Fred Neumann <fred.neumann@fau.de>
2022-10-27 00:00:56 +0200

    improved tiny cleanup and paste behaviour

--- src/components/Essay.vue
+++ src/components/Essay.vue
@@ -17,6 +17,7 @@ import contentLocalCss from '@/styles/content.css';
 /* Import plugins */
 import 'tinymce/plugins/lists';
 import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/paste';
 /* Import tiny vue integration */
 import Editor from '@tinymce/tinymce-vue'
 
@@ -30,16 +31,44 @@ function toolbar() {
   switch (settingsStore.formatting_options)
   {
     case 'full':
-      return 'undo redo | formatselect | bold italic underline | bullist numlist | removeformat | charmap';
+      return 'undo redo | formatselect | bold italic underline | bullist numlist | removeformat | charmap | paste';
     case 'medium':
-      return 'undo redo | bold italic underline | bullist numlist | removeformat | charmap';
+      return 'undo redo | bold italic underline | bullist numlist | removeformat | charmap | paste';
     case 'minimal':
-      return 'undo redo | bold italic underline | removeformat | charmap';
+      return 'undo redo | bold italic underline | removeformat | charmap | paste';
     case 'none':
     default:
-      return 'undo redo | charmap';
+      return 'undo redo | charmap |paste';
   }
 }
+
+/**
+ * @see https://www.tiny.cloud/docs/configure/content-filtering/#valid_elements
+ */
+function validElements() {
+  switch (settingsStore.formatting_options)
+  {
+    case 'full':
+      return 'p/div,br,strong/b,em/i,u,ol,ul,li,h1,h2,h3,h4,h5,h6,pre';
+    case 'medium':
+      return 'p/div,br,strong/b,em/i,u,ol,ul,li';
+    case 'minimal':
+      return 'p/div,p/li,br,strong/b,em/i,u';
+    case 'none':
+    default:
+      return 'p/div,p/li,br';
+  }
+}
+
+/**
+ * @see https://www.tiny.cloud/docs/configure/content-formatting/#formats
+ */
+function formats() {
+  return {
+    underline: {inline: 'u', remove: 'all'}
+  }
+}
+
 
 // Used for retrieving the editor instance using the tinymce.get('ID') method.
 const id = "essay";
@@ -55,12 +84,15 @@ const id = "essay";
       :init="{
         height: '100%',
         menubar: false,
-        plugins: 'lists charmap',
+        plugins: 'lists charmap paste',
         toolbar: toolbar(),
+        valid_elements: validElements(),
+        formats: formats(),
         custom_undo_redo_levels: 10,
-        skin: false,                //  avoid 404 errors for skin css files
-        content_css: false,              // avoid 404 error for content css file
-        content_style: contentUiCss.toString() + '\n' + contentLocalCss.toString()
+        skin: false,                      //  avoid 404 errors for skin css files
+        content_css: false,               // avoid 404 error for content css file
+        content_style: contentUiCss.toString() + '\n' + contentLocalCss.toString(),
+        paste_block_drop: true
        }"
   />
 </template>
